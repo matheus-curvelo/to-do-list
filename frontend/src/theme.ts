c4a2f84261f18f9b6ce5cdb7a6bb56a03c9f7fe4
Matheus Curvelo <83092595+matheus-curvelo@users.noreply.github.com>
2024-06-27 15:14:33 -0300

    Atualizando variáveis

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -3,10 +3,16 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1976d2',
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
     },
     secondary: {
-      main: '#f50057',
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
     },
   },
 });
